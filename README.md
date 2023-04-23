cab403e5872f565e73e24b0d0472735796776d13
Bryant Biggs <bryantbiggs@gmail.com>
2023-04-23 15:04:02 -0400

    feat: Add support for configuring a custom port and/or path

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # up
 
 You up?
+
+`up` checks if a local server is healthy by performing an HTTP GET request at the specified path and port. If the server responds with a 200 OK, `up` exits with a 0 status code. Otherwise, `up` exits with a 1 status code, indicating the health check has failed.
+
+Why is this useful? Amazon ECS provides support for health checking of containers defined in ECS Tasks, but it does not provide a client, nor the facilities, to perform the health check request. Instead, users must add a client, such as `curl`, to their container image in order to support the health check. `curl` is an amazing tool, but quite a bit more than whats necessary for a simple health check. Therefore, `up` was created - to simply ask "Hey, you up?"
+
+## Usage
+
+By default, `up` checks for a HTTP 200 OK response on localhost port `80` at the path `/healthz`. The port and/or path, can be changed via environment variables or arguments.
+
+### Environment Variables
+
+`UP_PATH` and `UP_PORT` environment variables will modify the path and port that `up` checks.
+
+```sh
+UP_PATH=/healthy UP_PORT=3000 up
+```
+
+### Arguments
+
+Using arguments, the first argument is the path, and the second argument is the port:
+
+```sh
+up /healthy 3000
+```
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
 use std::{
+  env,
   io::{Read, Write},
   net::{Shutdown, SocketAddr, TcpStream},
 };
 
+fn get_path(args: &[String]) -> String {
+  match env::var("UP_PATH") {
+    Ok(path) => path,
+    Err(_) => {
+      if args.len() > 2 {
+        args[1].to_string()
+      } else {
+        "/healthz".to_string()
+      }
+    }
+  }
+}
+
+fn get_port(args: &[String]) -> u16 {
+ match env::var("UP_PORT") {
+    Ok(port) => port.parse::<u16>().unwrap(),
+    Err(_) => {
+      if args.len() > 2 {
+        args[2].parse::<u16>().unwrap()
+      } else {
+        8080
+      }
+    }
+  }
+}
+
 fn main() -> std::io::Result<()> {
-  // Connect to server
-  let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
+  let args = env::args().collect::<Vec<String>>();
+
+  let port = get_port(&args);
+  let addr = SocketAddr::from(([127, 0, 0, 1], port));
   let mut stream = TcpStream::connect(addr)?;
 
   // Create GET request
-  let path = "/healthz";
+  let path = get_path(&args);
   let mut http_req = String::new();
   http_req.push_str(&format!("GET {} HTTP/1.1\r\n", path));
   http_req.push_str(&format!("Host: {}\r\n", addr));
@@ -18,7 +47,7 @@ fn main() -> std::io::Result<()> {
 
   // Send request
   let req_bytes = http_req.as_bytes();
-  stream.write_all(&req_bytes)?;
+  stream.write_all(req_bytes)?;
 
   // Parse response
   let mut response = String::new();
